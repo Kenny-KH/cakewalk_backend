0576f4386528b26408ba125049ecdb8ed36c4be4
Sun Ho <tjsgh5312@daum.net>
2022-07-31 16:01:41 +0900

    complete cake models.py

--- cake/models.py
+++ cake/models.py
@@ -1,6 +1,5 @@
-from secrets import choice
-from sys import maxsize
 from tkinter import CASCADE
+from unittest.util import _MAX_LENGTH
 from django.core.validators import RegexValidator
 from django.db import models
 
@@ -45,13 +44,42 @@ class Like_Cake(models.Model):
     store_cake_id = models.ForeignKey('Store_Cake', on_delete=CASCADE)
 
 class Store_Order(models.Model):
-
-    store_order_id = models.IntegerField()    
+    store_order_id = models.IntegerField(primary_key=True)    
     user_id = models.ForeignKey('User', on_delete=CASCADE)
     store_cake_id = models.ForeignKey('Store_Cake', on_delete=CASCADE)
     order_date = models.DateTimeField()
     OPTION_CHOICES = ['1호', '2호', '3호', '기타']
-    option_size = models.TextChoices(maxsize=2, choice=OPTION_CHOICES)
-    OPTION_FLAVOR = ['바닐라']
+    option_size = models.TextField(max_length=2, choices=OPTION_CHOICES)
+    OPTION_FLAVOR = ['바닐라', '초코', '생크림', '기타']
+    option_flavor = models.TextField(max_length=3, choices=OPTION_FLAVOR)
+    OPTION_ADDTION = ['초', '풍선', '보냉백']
+    addtional_option = models.TextField(max_length=3, choices=OPTION_ADDTION)
+
+
+class User_Order(models.Model):
+    user_order_id = models.IntegerField(primary_key=True)    
+    user_id = models.ForeignKey('User', on_delete=CASCADE)
+    store_cake_id = models.ForeignKey('Store_Cake', on_delete=CASCADE)
+    order_date = models.DateTimeField()
+    OPTION_CHOICES = ['1호', '2호', '3호', '기타']
+    option_size = models.TextField(max_length=2, choices=OPTION_CHOICES)
+    OPTION_FLAVOR = ['바닐라', '초코', '생크림', '기타']
+    option_flavor = models.TextField(max_length=3, choices=OPTION_FLAVOR)
+    OPTION_ADDTION = ['초', '풍선', '보냉백']
+    addtional_option = models.TextField(max_length=3, choices=OPTION_ADDTION)
+
+
+class User (models.Model):
+    User_id = models.IntegerField()
+    nick_name = models.CharField(max_length=50)
+    telNumberRegex = RegexValidator(regax = r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
+    tel = models.CharField(validators = [telNumberRegex], max_length=11, unique=True)
+    photo = models.ImageField(upload_to="appname", null=True)
+    # kakao_token =
+    
+class User_Cake (models.Model):
+    user_cake_id = models.IntegerField()
+    image_url = models.URLField(max_length=200)
+    create_date = models.DateTimeField()
 
 
