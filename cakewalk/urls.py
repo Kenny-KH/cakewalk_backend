4625cd45ba340e8fc836a097a386edb5ed39e2fd
Kenny-KH <77623474+Kenny-KH@users.noreply.github.com>
2022-08-12 22:54:18 +0900

    [FEAT] Account urls

--- cakewalk/urls.py
+++ cakewalk/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from store import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('create', views.create, name="create"),
     path('store/', include('store.urls')),
     path('user/', include('user.urls')),
-    path('account/', include('account.urls'))
+    path('cake/', include('cake.urls')),
+    path('account/', include('account.urls')),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
