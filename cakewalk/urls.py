61b33b41d69543df6ad1ad461a82838644396593
Soyuly <soyul8147@gmail.com>
2022-08-12 22:45:41 +0900

    base.html

--- cakewalk/urls.py
+++ cakewalk/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django import views
 from django.contrib import admin
 from django.urls import path, include
-from store import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
     # path('create', views.create, name="create"),
     path('store/', include('store.urls')),
     path('user/', include('user.urls'))
