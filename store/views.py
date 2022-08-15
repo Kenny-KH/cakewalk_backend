c641ab1f7df526634ee393b5d2f5bdd4aa68305a
kimyou1102 <yoosk5485@naver.com>
2022-08-15 21:24:21 +0900

    휴무일선택 부분 추가 및 input tpye=file 수정

--- store/views.py
+++ store/views.py
@@ -7,32 +7,59 @@ from account.models import BsSignupDetail
 def manage(request):
     return render(request,'manage.html')
 
-def manageStore(request):
-    user = request.user
-    store = get_object_or_404(BsSignupDetail, author=user)
-    store_cake = StoreCake() 
-    if request.method == 'POST':
-        store.notice = request.POST['notice']
-        store_cake.store = user
-        store_cake.name = request.POST['name']
-        if 'sizemini' in request.POST:
-            store_cake.price_mini = request.POST['price_mini']
+# def manageStore(request):
+#     user = request.user
+#     store = get_object_or_404(BsSignupDetail, author=user)
+#     store_cake = StoreCake() 
+#     if request.method == 'POST':
+#         store.notice = request.POST['notice']
+#         store_cake.store = user
+#         store_cake.name = request.POST['name']
+#         if 'sizemini' in request.POST:
+#             store_cake.price_mini = request.POST['price_mini']
             
-        if 'size1' in request.POST:
-            store_cake.price_1 = request.POST['price_1']
+#         if 'size1' in request.POST:
+#             store_cake.price_1 = request.POST['price_1']
             
-        if 'size2' in request.POST:
-            store_cake.price_2 = request.POST['price_2']
+#         if 'size2' in request.POST:
+#             store_cake.price_2 = request.POST['price_2']
             
-        if 'size3' in request.POST:
-            store_cake.price_3 = request.POST['price_3']                                    
-        store_cake.image = request.FILES['image']
-        store.save()
-        store_cake.save()
-        messages.add_message(request, messages.SUCCESS,'성공적으로 수정 완료 되었습니다!')
+#         if 'size3' in request.POST:
+#             store_cake.price_3 = request.POST['price_3']                                    
+#         store_cake.image = request.FILES['image']
+#         store.save()
+#         store_cake.save()
+#         messages.add_message(request, messages.SUCCESS,'성공적으로 수정 완료 되었습니다!')
     
-        return redirect('/store/manage_store')
-    return render(request,'manage_store.html', {'notice' : store.notice})
+#         return redirect('/store/manage_store')
+#     return render(request,'manage_store.html', {'notice' : store.notice})
+def manageStore(request):
+    
+    # user = request.user
+    # store = get_object_or_404(BsSignupDetail, author=user)
+    # store_cake = StoreCake() 
+    # if request.method == 'POST':
+    #     store.notice = request.POST['notice']
+    #     store_cake.store = user
+    #     store_cake.name = request.POST['name']
+    #     if 'sizemini' in request.POST:
+    #         store_cake.price_mini = request.POST['price_mini']
+            
+    #     if 'size1' in request.POST:
+    #         store_cake.price_1 = request.POST['price_1']
+            
+    #     if 'size2' in request.POST:
+    #         store_cake.price_2 = request.POST['price_2']
+            
+    #     if 'size3' in request.POST:
+    #         store_cake.price_3 = request.POST['price_3']                                    
+    #     store_cake.image = request.FILES['image']
+    #     store.save()
+    #     store_cake.save()
+    #     messages.add_message(request, messages.SUCCESS,'성공적으로 수정 완료 되었습니다!')
+    
+    #     return redirect('/store/manage_store')
+    return render(request,'manage_store.html', )
 
 def manage6(request):
     return render(request,'manage6.html')
