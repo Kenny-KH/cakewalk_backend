9d00effe1ba996288f0d850637618f0a56c08f33
Kenny-KH <77623474+Kenny-KH@users.noreply.github.com>
2022-08-17 02:18:11 +0900

    making models

--- cake/models.py
+++ cake/models.py
@@ -4,17 +4,15 @@ from datetime import datetime
 
 from django.forms import CharField
 
-
 # Create your models here.
-class Store(models.Model):
-    tax = models.CharField(max_length=30)
-    businessNum = models.IntegerField()
-    type = models.CharField(max_length=50)
-    bsName = models.CharField(max_length=100)
-    repName = models.CharField(max_length=12)
-    birth = models.IntegerField()
-    phoneNum = models.IntegerField()
-    address = models.TextField()
-    registeration = models.ImageField()
-    report = models.ImageField()
-    
+class order(models.Model):
+#    mapSearch = models.ForeignKey(on_delete = models.CASCADE)
+    orderDate = models.DateTimeField()
+    cakeImg = models.ImageField()
+    cakeSize = models.CharField(max_length=10)
+    cakeTaste = models.CharField(max_length=50)
+    addOptions = models.CharField(max_length=50)
+    cakeRequest = models.TextField(max_length=500, blank=True)
+
+    class Meta:
+        db_table = 'map'
