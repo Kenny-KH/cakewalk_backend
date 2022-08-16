1b5b44d1806862873c6f158f8d8f0184a52bd9f0
kimyou1102 <yoosk5485@naver.com>
2022-08-16 17:28:04 +0900

    path연결 및 상속

--- user/views.py
+++ user/views.py
@@ -7,3 +7,18 @@ def myPage(request):
 def myPage2(request):
     return render(request, "mypage2.html")
 
+def myPage3(request):
+    return render(request, "mypage3.html")
+
+def myPage4(request):
+    return render(request, "mypage4.html")
+
+def myPage5(request):
+    return render(request, "mypage5.html")
+
+def myPage6(request):
+    return render(request, "mypage6.html")
+
+def myPage7(request):
+    return render(request, "mypage7.html")
+
