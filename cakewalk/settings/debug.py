03a7e5b895743e9463d101a189bdf115b35fa12e
Soyuly <soyul8147@gmail.com>
2022-08-18 21:05:37 +0900

    db수정

--- cakewalk/settings/debug.py
+++ cakewalk/settings/debug.py
@@ -11,6 +11,6 @@ DATABASES = {
 
 KAKAO_CONFIG ={
     "KAKAO_REST_API_KEY":'b0ed2f28699406d89d7290e89c5ae081',
-    "KAKAO_REDIRECT_URI":'https://port-0-cakewalk-backend-1i9hi4d24l6qowkrv.gksl1.cloudtype.app/account/login/kakao/callback/',
+    "KAKAO_REDIRECT_URI":'http://localhost:8000/account/login/kakao/callback/',
     "KAKAO_CLIENT_SECRET_KEY":"vXFeq1ibnjclm4UsBG0cGdPpioFk23on",
 }
