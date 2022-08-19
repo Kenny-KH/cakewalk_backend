a10019aa2d3e993ae22c0891c25187842ea551e0
Soyuly <soyul8147@gmail.com>
2022-08-20 00:30:33 +0900

    이미지넘겨주기

--- cake/urls.py
+++ cake/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns =[
     path('make/', views.make, name='make'),
+    path('make2/<int:id>', views.make2, name='make2'),
     path('order/<str:whatCake>/<int:cake_id>', views.order, name='order'),
     path('payment/<int:order_id>', views.payment, name='payment'),
    
